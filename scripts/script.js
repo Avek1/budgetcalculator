1c8dc3575786baecc679894fffe1b1a27fd5beeb
Juan Flores <jlfloresjr@gmail.com>
2018-09-16 16:30:40 -0700

    basic inputs, output, button, addition function work.  need to swap for percentage math.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
-var budgetCalculator = {};
+var budget = {};
 
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("A wasap, dahg!");
+    $("#btnCalculate").on("click", budget.calculate);
 })
+
+
+// var percentOfIncomeSaving;
+// var percentOfIncomeWant;
+
+budget.calculate = function () {
+    console.log("this is the calculate function");
+    var income = parseInt($("#inputIncome").val());
+    var percent = parseInt($("#inputPercent").val());
+    var output = income + percent;
+    console.log(output);
+    $("#outputAllocation").text(output);
+
+
+}
