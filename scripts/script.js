9b9e37c7e13263f19a7fe60f805cfd2f8a4b3e7a
Juan Flores <jlfloresjr@gmail.com>
2018-09-16 16:50:12 -0700

    replaced addition math for percentage math.  still testing it.

--- scripts/script.js
+++ scripts/script.js
@@ -11,9 +11,11 @@ $(document).ready(function () {
 
 budget.calculate = function () {
     console.log("this is the calculate function");
-    var income = parseInt($("#inputIncome").val());
-    var percent = parseInt($("#inputPercent").val());
-    var output = income + percent;
+    var income = parseFloat($("#inputIncome").val());
+    var percent = parseFloat($("#inputPercent").val());
+    // var output = income + percent;
+    // var output = percent*100/income;
+    var output = (percent / 100) * income;
     console.log(output);
     $("#outputAllocation").text(output);
 
