042bcd0e35e76682d7edf3f2e3f132c113813d96
Juan Flores <jlfloresjr@gmail.com>
2018-09-19 19:02:19 -0700

    implemented output and palance calculations

--- scripts/script.js
+++ scripts/script.js
@@ -13,11 +13,14 @@ budget.calculate = function () {
     console.log("this is the calculate function");
     var income = parseFloat($("#inputIncome").val());
     var percent = parseFloat($("#inputPercent").val());
-    // var output = income + percent;
-    // var output = percent*100/income;
     var output = (percent / 100) * income;
+    var balance = income - output;
     console.log(output);
     $("#outputAllocation").text(output);
+    console.log(balance);
+    $("#outputBalance").text(balance);
+
+
 
 
 }
